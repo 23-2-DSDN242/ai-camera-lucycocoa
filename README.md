1a45371e39912deb7109e81969f5219f2bb9ffde
lucycocoa <140350024+lucycocoa@users.noreply.github.com>
2024-05-28 16:36:25 +1200

    q

--- README.md
+++ README.md
@@ -2,4 +2,58 @@
 [![Open in Visual Studio Code](https://classroom.github.com/assets/open-in-vscode-718a45dd9cf7e7f842a935f5ebbe5719a5e09af4491e668f4dbf3b35d5cca122.svg)](https://classroom.github.com/online_ide?assignment_repo_id=15040891&assignment_repo_type=AssignmentRepo)
 ## Creative Coding 2: Custom Pixel
 
-The images above are photos of Wellington CBD algorithmically processed with a lightly modified version of the [p5.js Pointillism example](https://p5js.org/examples/image-pointillism.html). The masks for each photo highlight various objects. Replace this text with your own which explains the source of your photos, masking, and applied algorithm.
+
+# First experiment
+
+I started experimenting with rabbit hole photos I took during last year's photography class on the theme of 'space.' Since I haven't decided on a specific theme yet, I used the Pointillism code provided in the coding examples to process the rabbit hole photos.
+
+![coding](https://github.com/23-2-DSDN242/ai-camera-lucycocoa/assets/140350024/2c4a1b10-f7d4-426e-bcdd-b8a7153a801e)
+
+However, the drawback of these photos was their lack of distinct features. This leads errors occurred in generating new masks from other images using AI, even though the shapes were simple, the AI failed to accurately create the masks.
+
+# Second experiment
+
+Instead of using the rabbit hole photos, I decided it would be better to use phtos with more distinct and characteristic objects. So, I began experimenting again with photos of dogs. Dog photos have clear shapes and features, which I believed would be more suitable for AI to generate masks accurately.
+
+Next, I used the Pointillism code to process the images. I wanted the dog to have a distinct, contrasting feel against the background, so I increased the number of iterations in the loop, the loop was set to for(let i=0; i<4000; i++), but I changed it to for(let i=0; i<100000; i++). Also I used a code that rendered only the masked part(dog) in grayscale.
+
+
+````
+for(let i=0;i<100000;i++) {
+  
+    
+    let x = floor(random(sourceImg.width));
+    let y = floor(random(sourceImg.height));
+    let pix = sourceImg.get(x, y);
+    let mask = maskImg.get(x, y);
+    fill(pix);
+
+    // Convert to grayscale
+
+
+    if(mask[0] > 128) {
+      let gray = (pix[0] + pix[1] + pix[2]) / 3;
+      let grayPix = [gray, gray, gray];
+      fill(grayPix);
+      let pointSize = 20;
+      rect(x, y, pointSize, pointSize);
+    
+    }
+    else {
+      
+      let pointSize = 10;
+      fill(sourceImg.get(x,y))
+      rect(x, y, pointSize, pointSize);
+      
+    }
+  }
+
+  ````
+
+  From left to right: when for(let i=0; i<4000; i++), when for(let i=0; i<100000; i++), and when the mask is in grayscale.
+
+  ![readme](https://github.com/23-2-DSDN242/mddn-242-data-mapping-dribnet/assets/140350024/ca9563f5-3275-4128-b4c3-2e776e2d339d)
+
+
+
+
--- sketch.js
+++ sketch.js
@@ -1,11 +1,124 @@
+// let sourceImg=null;
+// let maskImg=null;
+// let renderCounter=0;
+
+// // change these three lines as appropiate
+// let sourceFile = "input_3.jpg";
+// let maskFile   = "mask_3.png";
+// let outputFile = "output_1.png";
+
+// function preload() {
+//   sourceImg = loadImage(sourceFile);
+//   maskImg = loadImage(maskFile);
+// }
+
+// function setup () {
+//   let main_canvas = createCanvas(1920, 1080);
+//   main_canvas.parent('canvasContainer');
+
+//   imageMode(CENTER);
+//   noStroke();
+//   background(255, 0, 0);
+//   sourceImg.loadPixels();
+//   maskImg.loadPixels();
+
+  
+
+  
+
+//   // sourceImg.filter(BLUR, 8)
+//   // for(var r = 0; r <= 256; r += 64) {
+//   //   for(var g = 0; g <= 256; g += 64) {
+//   //     for(var b = 0; g <= 256; g += 64){
+//   //       colors.push([r,g,b])
+//   //     }
+//   //   }
+//   // }
+
+//   // Img.loadPixels()
+//   // for(var x = 0; x < sourceImg.width; x++) {
+//   //   for(var y =0; y < sourceImg.height; y++){
+
+//   //     var index = ( x+ y * maskImg.width)* 4
+//   //     var r =  maskImg.pixels[index +0]
+//   //     var g =  maskImg.pixels[index +1]
+//   //     var b =  maskImg.pixels[index +2]
+
+//   //     var closestColor = [3 * 255, []]
+//   //     for (color of colors) {
+//   //       var diff = abs(color[0]-r +abs(color[1]-g +abs(color[2]-b)))
+//   //       if (diff < closestColor[0]){
+//   //         closestColor[0]= diff
+//   //         closestColor[1]= color
+//   //       }
+//   //     }
+//   //     maskImg.pixels[index +0] = closestColor[1][0]
+//   //     maskImg.pixels[index +1] = closestColor[1][1]
+//   //     maskImg.pixels[index +2] = closestColor[1][2]
+//   //    }
+//   // }
+// }
+
+// function draw () {
+//   for(let i=0;i<4000;i++) {
+//     let x = floor(random(sourceImg.width));
+//     let y = floor(random(sourceImg.height));
+//     let pix = sourceImg.get(x, y);
+//     let mask = maskImg.get(x, y);
+//     fill(pix);
+//     if(mask[0] > 128) {
+//       let pointSize = 10;
+//       ellipse(x, y, pointSize, pointSize);
+//     }
+//     else {
+//       let pointSize = 20;
+//       rect(x, y, pointSize, pointSize);    
+//     }
+
+
+
+
+    
+//   }
+
+
+
+  
+//   renderCounter = renderCounter + 1;
+//   if(renderCounter > 10) {
+//     console.log("Done!")
+//     noLoop();
+//     // uncomment this to save the result
+//     // saveArtworkImage(outputFile);
+//   }
+// }
+
+// function keyTyped() {
+//   if (key == '!') {
+//     saveBlocksImages();
+//   }
+// }
+
+
+
+///위엥거!!
+
+
+
+
+
+
+
+
 let sourceImg=null;
 let maskImg=null;
-let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "input_1.jpg";
-let maskFile   = "mask_1.png";
+let sourceFile = "input_11.jpg";
+let maskFile   = "mask_11.png";
 let outputFile = "output_1.png";
+
+let colors =[];
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -18,13 +131,92 @@ function setup () {
 
   imageMode(CENTER);
   noStroke();
-  background(255, 0, 0);
-  sourceImg.loadPixels();
+  background(0, 0, 128);
+  // sourceImg.loadPixels();
   maskImg.loadPixels();
+  sourceImg.loadPixels();
+  colorMode(RGB);
+
+
 }
 
+
+//   sourceImg.filter(BLUR, 8);
+ 
+
+ 
+//  for(let r = 0; r <= 256; r += 64) {
+//     for(let g = 0; g <= 256; g += 64) {
+//       for(let b = 0; b <= 256; b += 64) {
+//         colors.push([r,g,b])
+//       }
+//     }
+//   }
+
+//   sourceImg.loadPixels()
+//   for(let x = 0; x < sourceImg.width; x++) {
+//     for(let y =0; y < sourceImg.height; y++){
+
+//       let index = ( x+ y * sourceImg.width)* 4
+//       let r =  sourceImg.pixels[index +0]
+//       let g =  sourceImg.pixels[index +1]
+//       let b =  sourceImg.pixels[index +2]
+
+//       let closestColor = [3 * 255, []]
+//       for (color of colors) {
+//         let diff = abs(color[0]-r) +abs(color[1]-g) +abs(color[2]-b)
+//         if (diff < closestColor[0]){
+//           closestColor[0]= diff
+//           closestColor[1]= color
+//         }
+//       }
+//       sourceImg.pixels[index +0] = closestColor[1][0]
+//       sourceImg.pixels[index +1] = closestColor[1][1]
+//       sourceImg.pixels[index +2] = closestColor[1][2]
+//      }
+//   }
+//   sourceImg.updatePixels();
+
+
+// let X_STOP = 640;
+// let Y_STOP = 480;
+let X_STOP = 1920;
+let Y_STOP = 1080;
+let OFFSET = 20;// change this to 200 for more noticeable effect
+
+let renderCounter=0;
+
+
 function draw () {
-  for(let i=0;i<4000;i++) {
+  
+//   // angleMode(DEGREES);
+//   // let num_lines_to_draw = 40;
+//   // // get one scanline
+//   // for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
+//   //   for(let i=0; i<X_STOP; i++) {
+//   //     colorMode(RGB);
+//   //     let mask = maskImg.get(i, j);
+//   //     if (mask[1] < 128) {
+//   //       pix = sourceImg.get(i, j);
+//   //     }
+//   //     else {
+//   //       let wave = sin(j*3);/////// *8
+//   //       let slip = map(wave, -1, 1, -OFFSET, OFFSET);
+//   //       pix = sourceImg.get(i+slip, j);
+
+//   //       // let brt = map(wave, -1, 1, 0, 255);
+//   //       // for(let c=0; c<3; c++) {
+//   //       //   pix[c] = brt;
+//   //       // }
+//   //     }
+
+//   //     set(i, j, pix);
+//   //   }
+//   // }
+ 
+  for(let i=0;i<100000;i++) {
+  
+    
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
@@ -32,24 +224,92 @@ function draw () {
     fill(pix);
     if(mask[0] > 128) {
       let pointSize = 10;
-      ellipse(x, y, pointSize, pointSize);
+      fill(sourceImg.get(x,y))
+      rect(x, y, pointSize, pointSize);
     }
     else {
+      // Convert to grayscale
+      let gray = (pix[0] + pix[1] + pix[2]) / 3;
+      let grayPix = [gray, gray, gray];
+      fill(grayPix);
       let pointSize = 20;
-      rect(x, y, pointSize, pointSize);    
+      rect(x, y, pointSize, pointSize);
     }
   }
-  renderCounter = renderCounter + 1;
-  if(renderCounter > 10) {
-    console.log("Done!")
-    noLoop();
-    // uncomment this to save the result
-    // saveArtworkImage(outputFile);
-  }
+  
+
+//   // renderCounter = renderCounter + num_lines_to_draw;
+//   // updatePixels();
+
+//   // print(renderCounter);
+//   if(renderCounter > Y_STOP) {
+//     console.log("Done!")
+//     noLoop();
+//     // uncomment this to save the result
+//     // saveArtworkImage(outputFile);
+//   }
+// }
+
+// function keyTyped() {
+//   if (key == '!') {
+//     saveBlocksImages();
+//   }
+// }
+
+
+
+
+
+// //make mask image into grayscale
+
+// angleMode(DEGREES);
+// let num_lines_to_draw = 40;
+// // Get one scanline
+// for (let j = renderCounter; j < renderCounter + num_lines_to_draw && j < Y_STOP; j++) {
+//   for (let i = 0; i < X_STOP; i++) {
+//     colorMode(RGB);
+//     let mask = maskImg.get(i, j);
+//     let pix = sourceImg.get(i, j);
+
+//     if (mask[1] < 128) {
+//       // Convert to grayscale
+//       let wave = sin(j * 5);
+//       let slip = map(wave, -1, 1, -OFFSET, OFFSET);
+//       pix = sourceImg.get(i + slip, j);
+      
+//     } else {
+//       let gray = (pix[0] + pix[1] + pix[2]) /3 ;
+//       pix = [gray, gray, gray];
+//     }
+
+//     set(i, j, color(pix));
+//   }
+// }
+// renderCounter += num_lines_to_draw;
+// updatePixels();
+
+if (renderCounter > Y_STOP) {
+  console.log("Done!");
+  noLoop();
+  // Uncomment this to save the result
+  // saveArtworkImage(outputFile);
+}
 }
 
 function keyTyped() {
-  if (key == '!') {
-    saveBlocksImages();
-  }
+if (key == '!') {
+  saveCanvas('myCanvas', 'png');
 }
+}
+
+
+
+
+
+
+
+
+
+
+
+// ///////////coloured blur
